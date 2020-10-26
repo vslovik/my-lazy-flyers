cd4b3d7f9ad0ae676f2c9e328dd9dbcf74e1f774
Valeriya <vslovik@gmail.com>
2020-10-26 02:28:32 +0100

    flyers lazy loading (wip)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col';
 import Flyer from './Flyer';
 import Top from './Top';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import axios from 'axios';
 
 function App() {
 
@@ -17,6 +18,73 @@ function App() {
     setShow(flag)
   }
 
+	const [listItems, setListItems] = useState([]);
+	const [isFetching, setIsFetching] = useState(false);
+	const [page, setPage] = useState(1);
+
+	useEffect(() => {
+		fetchData();
+		window.addEventListener('scroll', handleScroll);
+	}, []);
+
+	const handleScroll = () => {
+		if (
+			Math.ceil(window.innerHeight + document.documentElement.scrollTop) !== document.documentElement.offsetHeight ||
+			isFetching
+		)
+			return;
+		setIsFetching(true);
+		console.log(isFetching);
+	};
+
+  const fetchData = async () => {
+      const result = await axios.get(`http://127.0.0.1:3000/flyers?page=${page}&limit=8`); //ToDo: move url to config
+      let data = [];
+      if (result && result.data && result.data.data && result.data.data.length) { // ToDo: handle errors
+        data = JSON.parse(result.data.data);
+      }
+      console.log('LENGTH', data.length, data[0])
+			setPage(page + 1);
+			setListItems(() => {
+				return [...listItems, ...data];
+      });
+	};
+
+	useEffect(() => {
+		if (!isFetching) return;
+		fetchMoreListItems();
+	}, [isFetching]);
+
+	const fetchMoreListItems = () => {
+		fetchData();
+		setIsFetching(false);
+  };
+
+  // let flyers = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id:7}, {id:8}, {id:9}, {id:10}, {id:11}, {id:12}]
+  const numrows = Math.round(listItems.length / 4);
+  const numcols = 4;
+  let item;
+  let rows = [];
+  for (var r = 0; r < numrows; r++) {
+    let cols = [];
+    for (var c = 0; c < numcols; c++) {
+      item = listItems[r*4 + c]
+      if (item.retailer) {
+        cols.push(
+          <Col xs={6} sm={3}>
+            <Flyer 
+              id={item.id} 
+              title={item.title}
+              retailer={item.retailer}
+              category={item.category}
+              />
+          </Col>
+      )
+      };
+    }
+    rows.push(<Row>{cols}</Row>)
+  }
+
   return(
     <Container>
       <Navbar bg="dark" variant="dark">
@@ -24,35 +92,9 @@ function App() {
           <GiHamburgerMenu onClick={handleShow} /> Shopfully
         </Navbar.Brand>
       </Navbar>
-      <Row>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-        <Col xs={6} sm={3}>
-          <Flyer />
-        </Col>
-      </Row>
+      {rows}
       <Top show={show} handleTop={handleTop}/>
+      {isFetching && <h1>Fetching more flyers...</h1>}
     </Container>
   )
 }
--- src/Flyer.js
+++ src/Flyer.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Heart from './Heart';
 
-const Flyer = () => {
+const Flyer = (props) => {
   return(
         <Card>
             <Card.Img variant="top" src="./grey_md.jpg" />
             <Card.Body>
-                <Card.Text>RETAILER NAME</Card.Text>
+                <Card.Text>{props.retailer.toUpperCase()}</Card.Text>
                 <Card.Title>
-                    Flier Title
+                    {props.title}
                 </Card.Title>
-                <Card.Subtitle>Category Name</Card.Subtitle>
+                <Card.Subtitle>{props.category}</Card.Subtitle>
                 <Heart />
             </Card.Body>
         </Card>      
